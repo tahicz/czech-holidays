1253ad0057f45fcbe60a384be6f92d065c1af407
Jan Mlatec <tahicz@gmail.com>
2023-04-11 16:25:04 +0200

    Add phpcs and phpstan

--- src/CzechHolidays.php
+++ src/CzechHolidays.php
@@ -1,162 +1,184 @@
 <?php
+declare(strict_types=1);
 
-    namespace tahicz;
+namespace tahicz;
 
-    use DateInterval;
-    use DateTime;
-    use DateTimeImmutable;
-    use DateTimeInterface;
-    use Doctrine\Common\Collections\ArrayCollection;
-    use Exception;
-    use tahicz\Model\Holiday;
+use DateInterval;
+use DateTime;
+use DateTimeImmutable;
+use DateTimeInterface;
+use Doctrine\Common\Collections\ArrayCollection;
+use Exception;
+use tahicz\Model\Holiday;
 
-    class CzechHolidays
+class CzechHolidays
+{
+    private const INDEX_FORMAT = 'n-j';
+
+    /**
+     * List of holidays
+     *
+     * @var ArrayCollection<string,Holiday>
+     */
+    private ArrayCollection $_holidaysList;
+
+    /**
+     * Act No. 245/2000 Coll., on public holidays, other holidays,
+     * significant days and days off.
+     */
+    private const HOLIDAYS = [
+        '01-01' => 'Nový rok',
+        '05-01' => 'Svátek práce',
+        '05-08' => 'Den vítězství',
+        '06-05' => 'Den slovanských věrozvěstů Cyrila a Metoděje',
+        '06-06' => 'Den upálení mistra Jana Husa',
+        '09-28' => 'Den české státnosti',
+        '10-28' => 'Den vzniku samostatného československého státu',
+        '11-17' => 'Den boje za svobodu a demokracii',
+        '12-24' => 'Štědrý den',
+        '12-25' => '1. svátek vánoční',
+        '12-26' => '2. svátek vánoční'
+    ];
+
+    private const OPENING_ON_HOLIDAYS = [
+        '01-01' => Holiday::SHOP_CLOSE,
+        '05-08' => Holiday::SHOP_CLOSE,
+        '09-28' => Holiday::SHOP_CLOSE,
+        '10-28' => Holiday::SHOP_CLOSE,
+        '12-24' => Holiday::SHOP_OPEN_MORNING,
+        '12-25' => Holiday::SHOP_CLOSE,
+        '12-26' => Holiday::SHOP_CLOSE,
+    ];
+
+    private const GOOD_FRIDAY = 'Velký pátek';
+    private const EASTER_MONDAY = 'Velikonoční pondělí';
+
+    /**
+     * Czech holiday init
+     */
+    public function __construct()
     {
-        private const INDEX_FORMAT = 'n-j';
+        $this->_holidaysList = new ArrayCollection();
+    }
 
-        private ArrayCollection $holidaysList;
-
-        /**
-         * Act No. 245/2000 Coll., on public holidays, other holidays, significant days and days off.
-         */
-        private const HOLIDAYS = [
-            '01-01' => 'Nový rok',
-            '05-01' => 'Svátek práce',
-            '05-08' => 'Den vítězství',
-            '06-05' => 'Den slovanských věrozvěstů Cyrila a Metoděje',
-            '06-06' => 'Den upálení mistra Jana Husa',
-            '09-28' => 'Den české státnosti',
-            '10-28' => 'Den vzniku samostatného československého státu',
-            '11-17' => 'Den boje za svobodu a demokracii',
-            '12-24' => 'Štědrý den',
-            '12-25' => '1. svátek vánoční',
-            '12-26' => '2. svátek vánoční'
-        ];
-
-        private const OPENING_ON_HOLIDAYS = [
-            '01-01' => Holiday::SHOP_CLOSE,
-            '05-08' => Holiday::SHOP_CLOSE,
-            '09-28' => Holiday::SHOP_CLOSE,
-            '10-28' => Holiday::SHOP_CLOSE,
-            '12-24' => Holiday::SHOP_CLOSE,
-            '12-25' => Holiday::SHOP_CLOSE,
-            '12-26' => Holiday::SHOP_CLOSE,
-        ];
-
-        private const GOOD_FRIDAY   = 'Velký pátek';
-        private const EASTER_MONDAY = 'Velikonoční pondělí';
-
-        public function __construct()
-        {
-            $this->holidaysList = new ArrayCollection();
+    /**
+     * Check if given date is holiday or not
+     *
+     * @param DateTimeInterface $date Date for holiday check
+     *
+     * @return bool
+     * @throws Exception
+     */
+    public function isHoliday(DateTimeInterface $date): bool
+    {
+        if ($this->_holidaysList->isEmpty()) {
+            $this->_initHolidays($this->_holidaysList, null);
+        }
+        $current = new DateTimeImmutable();
+        $isStableHoliday = $this->_holidaysList->containsKey(
+            $date->format(self::INDEX_FORMAT)
+        );
+        if ($isStableHoliday) {
+            return true;
         }
 
-        /**
-         * @param DateTimeInterface $dateTime
-         *
-         * @return bool
-         * @throws Exception
-         */
-        public function isHoliday(DateTimeInterface $dateTime): bool
-        {
-            if ($this->holidaysList->isEmpty()) {
-                $this->initHolidays($this->holidaysList, null);
-            }
-            $current         = new DateTimeImmutable();
-            $isStableHoliday = $this->holidaysList->containsKey($dateTime->format(self::INDEX_FORMAT));
-            if ($isStableHoliday) {
-                return true;
-            }
+        $year = (int)$date->format('Y');
 
-            $year = $dateTime->format('Y');
-
-            if ($year !== $current->format('Y')) {
-                foreach ($this->getEasterDatetime($year) as $holiday) {
-                    if ($holiday->getDate() == $dateTime) {
-                        return true;
-                    }
+        if ($year !== (int)$current->format('Y')) {
+            foreach ($this->_getEasterDatetime($year) as $holiday) {
+                if ($holiday->getDate() == $date) {
+                    return true;
                 }
             }
-            return false;
-
         }
+        return false;
 
-        /**
-         * @param int|null $year
-         *
-         * @return ArrayCollection
-         * @throws Exception
-         */
-        public function getHolidays(?int $year): ArrayCollection
-        {
-            $current = new DateTimeImmutable();
-            if (empty($year)) {
-                $year = $current->format('Y');
-            }
-            if ($this->holidaysList->isEmpty()) {
-                $this->initHolidays($this->holidaysList, $year);
-            }
-            return $this->holidaysList;
+    }
+
+    /**
+     * Returns holidays list for given year
+     *
+     * @param int|null $year Year for holiday list
+     *
+     * @return ArrayCollection<string,Holiday>
+     * @throws Exception
+     */
+    public function getHolidays(?int $year): ArrayCollection
+    {
+        $current = new DateTimeImmutable();
+        if (empty($year)) {
+            $year = (int)$current->format('Y');
         }
-
-        /**
-         * @param ArrayCollection $list
-         * @param int|null        $year
-         *
-         * @return void
-         * @throws Exception
-         */
-        private function initHolidays(ArrayCollection $list, ?int $year): void
-        {
-            if (empty($year)) {
-                $year = (int)date('Y');
-            }
-            foreach (self::HOLIDAYS as $date => $name) {
-                if($date === '12-24'){
-                    $holiday = new Holiday(
-                        $name,
-                        new DateTimeImmutable($year . '-' . $date),
-                        false,
-                        key_exists($date, self::OPENING_ON_HOLIDAYS) ? self::OPENING_ON_HOLIDAYS[$date] : Holiday::SHOP_OPEN_MORNING
-                    );
-                } else {
-                    $holiday = new Holiday(
-                        $name,
-                        new DateTimeImmutable($year . '-' . $date),
-                        false,
-                        key_exists($date, self::OPENING_ON_HOLIDAYS) ? self::OPENING_ON_HOLIDAYS[$date] : Holiday::SHOP_OPEN
-                    );
-                }
-                $index   = $holiday->getDate()->format(self::INDEX_FORMAT);
-                $list->set($index, $holiday);
-            }
-            foreach ($this->getEasterDatetime($year) as $holiday) {
-                $index = $holiday->getDate()->format(self::INDEX_FORMAT);
-                $list->set($index, $holiday);
-            }
+        if ($this->_holidaysList->isEmpty()) {
+            $this->_initHolidays($this->_holidaysList, $year);
         }
+        return $this->_holidaysList;
+    }
 
-        /**
-         * @see https://www.php.net/manual/en/function.easter-date.php#refsect1-function.easter-date-notes
-         *
-         * @param int $year
-         *
-         * @return Holiday[]
-         * @throws Exception
-         */
-        private function getEasterDatetime(int $year): array
-        {
-            $base = new DateTime($year . '-03-21');
-            $days = easter_days($year);
-
-            $easterDay  = $base->add(new DateInterval('P' . $days . 'D')); //Velikonoční neděle, půlnoc
-            $goodFriday = DateTimeImmutable::createFromMutable(
-                $easterDay->modify('previous friday')
+    /**
+     * Generates Czech holidays list
+     *
+     * @param ArrayCollection<string,Holiday> $list Holiday list
+     * @param int|null                        $year Year of holidays
+     *
+     * @return void
+     * @throws Exception
+     */
+    private function _initHolidays(ArrayCollection $list, ?int $year): void
+    {
+        if (empty($year)) {
+            $year = (int)date('Y');
+        }
+        foreach (self::HOLIDAYS as $date => $name) {
+            if (key_exists($date, self::OPENING_ON_HOLIDAYS)) {
+                $openingRule = self::OPENING_ON_HOLIDAYS[$date];
+            } else {
+                $openingRule = Holiday::SHOP_OPEN;
+            }
+            $holiday = new Holiday(
+                $name,
+                new DateTimeImmutable($year . '-' . $date),
+                false,
+                $openingRule
             );
-
-            return [
-                new Holiday(self::GOOD_FRIDAY, $goodFriday, true),
-                new Holiday(self::EASTER_MONDAY, $goodFriday->modify('next monday'), true, Holiday::SHOP_CLOSE)
-            ];
+            $index = $holiday->getDate()->format(self::INDEX_FORMAT);
+            $list->set($index, $holiday);
+        }
+        foreach ($this->_getEasterDatetime($year) as $holiday) {
+            $index = $holiday->getDate()->format(self::INDEX_FORMAT);
+            $list->set($index, $holiday);
         }
     }
+
+    /**
+     * Calculate Easter holidays for given year
+     *
+     * @param int $year Year of Easter
+     *
+     * @return Holiday[]
+     * @throws Exception
+     *
+     * @see https://www.php.net/manual/en/function.easter-date.php#refsect1-function.easter-date-notes
+     */
+    private function _getEasterDatetime(int $year): array
+    {
+        $base = new DateTime($year . '-03-21');
+        $days = easter_days($year);
+
+        //Velikonoční neděle, půlnoc
+        $easterDay = $base->add(new DateInterval('P' . $days . 'D'));
+        $goodFriday = DateTimeImmutable::createFromMutable(
+            $easterDay->modify('previous friday')
+        );
+
+        return [
+            new Holiday(self::GOOD_FRIDAY, $goodFriday, true),
+            new Holiday(
+                self::EASTER_MONDAY,
+                $goodFriday->modify('next monday'),
+                true,
+                Holiday::SHOP_CLOSE
+            )
+        ];
+    }
+}
--- src/Model/Holiday.php
+++ src/Model/Holiday.php
@@ -1,90 +1,121 @@
 <?php
+declare(strict_types=1);
 
-    namespace tahicz\Model;
+namespace tahicz\Model;
 
-    use DateTimeInterface;
+use DateTimeInterface;
 
-    class Holiday
-    {
-        public const SHOP_CLOSE        = 'close';
-        public const SHOP_OPEN         = 'open';
-        public const SHOP_OPEN_MORNING = 'open_morning';
+class Holiday
+{
+    public const SHOP_CLOSE = 'close';
+    public const SHOP_OPEN = 'open';
+    public const SHOP_OPEN_MORNING = 'open_morning';
 
+    private ?string $_name;
+    private DateTimeInterface $_date;
+    private bool $_isEaster;
+    private string $_openingRule;
+    private string $_note = '';
 
-        /**
-         * @var string|null
-         */
-        private ?string $name = null;
-        /**
-         * @var DateTimeInterface|null
-         */
-        private ?DateTimeInterface $date = null;
-        private bool               $isEaster;
-        private string             $openingRule;
-        private string             $note = '';
-
-        public function __construct(string $name, DateTimeInterface $date, bool $isEaster = false, string $openingRule = self::OPEN)
-        {
-            $this->name        = $name;
-            $this->date        = $date;
-            $this->isEaster    = $isEaster;
-            $this->openingRule = $openingRule;
-        }
-
-        /**
-         * @return string|null
-         */
-        public function getName(): ?string
-        {
-            return $this->name;
-        }
-
-        /**
-         * @return DateTimeInterface|null
-         */
-        public function getDate(): ?DateTimeInterface
-        {
-            return $this->date;
-        }
-
-        /**
-         * @return bool
-         */
-        public function isEaster(): bool
-        {
-            return $this->isEaster;
-        }
-
-        /**
-         * @param string $note
-         *
-         * @return void
-         */
-        public function addNote(string $note): void
-        {
-            $this->note = $note;
-        }
-
-        public function getNote(): string
-        {
-            return $this->note;
-        }
-
-        /**
-         * @return string
-         */
-        public function getOpeningRule(): string
-        {
-            return $this->openingRule;
-        }
-
-        public function isShopsOpen(): bool
-        {
-            if ($this->getOpeningRule() === self::SHOP_OPEN) {
-                return true;
-            }
-
-            return false;
-        }
-
+    /**
+     * Holiday class
+     *
+     * @param string            $name        Name of holiday
+     * @param DateTimeInterface $date        Date of holiday
+     * @param bool              $isEaster    is Easter holiday
+     * @param string            $openingRule Shops openning flag
+     *                                       <ul>
+     *                                       <li>Holiday::SHOP_CLOSE</li>
+     *                                       <li>Holiday::SHOP_OPEN</li>
+     *                                       <li>Holiday::SHOP_OPEN_MORNING</li>
+     *                                       </ul>
+     */
+    public function __construct(
+        string            $name,
+        DateTimeInterface $date,
+        bool              $isEaster = false,
+        string            $openingRule = self::SHOP_OPEN
+    ) {
+        $this->_name = $name;
+        $this->_date = $date;
+        $this->_isEaster = $isEaster;
+        $this->_openingRule = $openingRule;
     }
+
+    /**
+     * Get holiday name
+     *
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->_name;
+    }
+
+    /**
+     * Get date of this holiday
+     *
+     * @return DateTimeInterface
+     */
+    public function getDate(): DateTimeInterface
+    {
+        return $this->_date;
+    }
+
+    /**
+     * Is this Easter holiday?
+     *
+     * @return bool
+     */
+    public function isEaster(): bool
+    {
+        return $this->_isEaster;
+    }
+
+    /**
+     * Add note to holiday
+     *
+     * @param string $note note
+     *
+     * @return void
+     */
+    public function addNote(string $note): void
+    {
+        $this->_note = $note;
+    }
+
+    /**
+     * Get note
+     *
+     * @return string
+     */
+    public function getNote(): string
+    {
+        return $this->_note;
+    }
+
+    /**
+     * Get information about shop opening rules
+     *
+     * @return string
+     */
+    public function getOpeningRule(): string
+    {
+        return $this->_openingRule;
+    }
+
+    /**
+     * Get information if shops are open by this holiday
+     *
+     * @return bool
+     */
+    public function isShopsOpen(): bool
+    {
+        if ($this->getOpeningRule() === self::SHOP_OPEN) {
+            return true;
+        }
+
+        return false;
+    }
+
+}
